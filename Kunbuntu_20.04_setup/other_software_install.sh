7fe9d9e1850f35e968df4bc01758b07d573cc207
AspirinZJ <zhangmengwei1996@outlook.com>
2022-03-10 16:11:56 -0500

    Added Kubuntu20.04 config file

---
+++ Kunbuntu_20.04_setup/other_software_install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -e
+
+sudo apt install texlive-full texstudio -y
+
+# file compare
+sudo apt install meld -y
+
+# qmmp is a lightweight music gui player
+sudo apt install qmmp -y
+
+# install guvcview for controlling camera and seeing the detials of the camera
+# guvcview is a gui tool for getting and setting the parameters of the camera
+sudo apt install guvcview -y
+
+# okular is a pdf viewer
+sudo apt install okular -y
+
+# gsmartcontrol is a S.M.A.R.T tool for hard drives and ssds
+sudo apt install gsmartcontrol -y
+
+# translation in shell
+sudo apt install translate-shell -y
+
+# color pickup on screen
+sudo snap install pick-colour-picker -y
+
